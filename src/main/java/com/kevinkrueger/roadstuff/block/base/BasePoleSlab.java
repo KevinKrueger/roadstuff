e53a4f55aceb731b25f18b443f94e9e253985aad
Kevin Krüger <Kevin22042001@gmail.com>
2021-04-03 21:38:08 +0200

    (Changed Functionality) -> BasePoleSlab
    (Model Changed)
    -> blockstates/streetlight_stoneslab.json
    
    (LootTable Changed)
    -> streetlight_stoneslab.json

--- src/main/java/com/kevinkrueger/roadstuff/block/base/BasePoleSlab.java
+++ src/main/java/com/kevinkrueger/roadstuff/block/base/BasePoleSlab.java
@@ -6,6 +6,8 @@ import com.kevinkrueger.roadstuff.util.ModBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.util.ActionResultType;
@@ -18,6 +20,7 @@ import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 
 import java.util.stream.Stream;
 
@@ -25,14 +28,13 @@ public class BasePoleSlab extends BlockBase
 {
 
     public static final BooleanProperty HAS_SIGN = BooleanProperty.create("has_sign");
-
     CalculateShape calc = new CalculateShape();
     private static final VoxelShape SHAPE_N = Stream.of(
             Block.makeCuboidShape(0, 0, 0, 16, 8, 7),
             Block.makeCuboidShape(0, 0, 9, 16, 8, 16),
             Block.makeCuboidShape(0, 0, 7, 7, 8, 9),
             Block.makeCuboidShape(9, 0, 7, 16, 8, 9)
-    ).reduce((v1, v2) -> {return VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR);}).get();
+    ).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR)).get();
 
     public BasePoleSlab(Properties properties)
     {
@@ -40,25 +42,27 @@ public class BasePoleSlab extends BlockBase
         setDefaultState(getDefaultState().with(HAS_SIGN, false));
         calc.runCalculation(SHAPE_N);
     }
+
     @Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context)
     {
         return calc.SHAPES.get(state.get(FACING));
     }
+
     @SuppressWarnings("deprecation")
     @Override
-    public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit)
+    public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos,
+                                             PlayerEntity player, Hand handIn, BlockRayTraceResult hit)
     {
-        if(worldIn.isRemote())
-        {
-            if(player.getHeldItemMainhand().getItem() == ModBlocks.STEELPOLE_LIGHTS.get().asItem())
-            {
+            if(player.getHeldItemMainhand().getItem() == ModBlocks.STEELPOLE_LIGHTS.get().asItem()) {
                 // Blockstate ändern
-                worldIn.setBlockState(pos, state.with(HAS_SIGN, true));
-                player.getHeldItemMainhand().shrink(1);
-            }
-        }
 
+                if(!worldIn.getBlockState(pos).get(HAS_SIGN))
+                {
+                    worldIn.setBlockState(pos, state.with(HAS_SIGN, true));
+                    player.getHeldItemMainhand().shrink(1);
+                }
+            }
         return ActionResultType.SUCCESS;
     }
 
@@ -68,7 +72,4 @@ public class BasePoleSlab extends BlockBase
         builder.add(HAS_SIGN);
         super.fillStateContainer(builder);
     }
-
-
-
 }
